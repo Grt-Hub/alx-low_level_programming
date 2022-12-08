4a6503a5fe9db310cc1d0a553b5c8a9579bfe974
Grt-Hub <other.grt@gmail.com>
2022-12-08 19:41:42 +0000

    Create a C program that prints a sentence with the function 'puts'

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Description: prints a sentence using function 'puts'
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
