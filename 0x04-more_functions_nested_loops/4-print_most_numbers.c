6a9fec2abb342d47c7be749b629e746f39edef51
Grt-Hub <other.grt@gmail.com>
2022-12-16 20:48:31 +0000

    Add a function that prints the numbers, from 0 to 9except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9except 2 and 4
+ *
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
