71360df4618abeff4f02e5f33f9772422a0d2345
Grt-Hub <other.grt@gmail.com>
2022-12-22 21:12:57 +0000

    Add a function that concatenates two strings like linux's 'strncat'

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes from src
+ *
+ * Return: a pointer to the resulting string `dest`
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i++])
+	{
+		j++;
+	}
+
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[j++] = src[i];
+	}
+
+	return (dest);
+}
